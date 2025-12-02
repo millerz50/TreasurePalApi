292a624545d1373c1cbf6c4089c3b02ded4d2732
Millerz50 <johwanisi1@gmail.com>
2025-12-02 22:49:00 +0200

    chore: remove compiled dist from repo; run ts-node in runtime

--- dist/utils/conditionalMiddleware.js
+++
@@ -1,9 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.withValidation = withValidation;
-/**
- * Conditionally apply validation middleware in production.
- */
-function withValidation(validator, handler, isProd) {
-    return isProd ? [validator, handler] : [handler];
-}
